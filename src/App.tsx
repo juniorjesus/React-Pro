b5f5b4b14b22be3659a175175f7086feaefb6035
junior lopez <juniorlopez3@live.com>
2022-01-26 22:08:22 -0500

    Manejar un objeto como estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Counter } from './bases/Counter';
+import { CounterBy } from './bases/CounterBy';
 
 function App() {
   return (
     <>
       <Counter initialValue = { 15 } />
+      
+      <CounterBy/>
     </>
   );
 }
